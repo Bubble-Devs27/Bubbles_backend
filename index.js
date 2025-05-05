b3ea05ed759951788a2c7766870d6b123acbf00a
Nihal-Sharma <72800662+Nihal-Sharma@users.noreply.github.com>
2025-05-05 16:47:51 +0530

    Privacy-policy

--- index.js
+++ index.js
@@ -5,6 +5,7 @@ const orderRouter = require('./Routes/order')
 const workerOrderRouter = require('./Routes/workerOrderRouter')
 const workerAuthRouter = require('./Routes/WorkerAuth')
 const cors = require("cors")
+const path = require('path');
 const app = express()
 const port = 5000
 app.use(express.json())
@@ -16,6 +17,11 @@ app.use("/worker/order" , workerOrderRouter)
 app.use("/worker/auth" , workerAuthRouter)
 app.get("/" ,  (req ,res)=>{res.json("Bubbles")})
 
+
+app.use(express.static(path.join(__dirname, 'public')));
+app.get('/privacy-policy', (req, res) => {
+  res.sendFile(path.join(__dirname, 'privacy-policy.html'));
+});
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`)
 })
